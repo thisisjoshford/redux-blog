96951f46ee0c6f945b19bf920eccfa63daeb5fc6
Josh Ford <thisisjoshford@gmail.com>
2020-05-11 22:54:37 -0700

    moved state to global

--- src/selectors/postSelectors.js
+++ src/selectors/postSelectors.js
@@ -1,2 +1,2 @@
-export const getAllPosts = state => (state);
+export const getAllPosts = state => state;
 
--- src/selectors/postSelectors.test.js
+++ src/selectors/postSelectors.test.js
@@ -1,21 +1,19 @@
 import { getAllPosts } from './postSelectors';
 
+const state = [
+  {
+    title: 'Blog Post Uno',
+    body: 'blog post body texty text...'
+  },
+  {
+    title: 'Blog Post Dos',
+    body: 'second super cool text...'
+  }
+];
+
 describe('post selectors', () => {
   it('gets all blog posts', () => {
-    const state = [
-      {
-        title: 'Blog Post Uno',
-        body: 'blog post body texty text...'
-      },
-      {
-        title: 'Blog Post Dos',
-        body: 'second super cool text...'
-      }
-    ];
-
     const posts = getAllPosts(state);
     expect(posts).toEqual(state);
-  }
-  );
+  });
 });
-
