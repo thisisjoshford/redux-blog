02b6ebdc15de4dde9941d4a9a6cf821f4dd709af
Josh Ford <thisisjoshford@gmail.com>
2020-05-11 22:43:42 -0700

    gets all blog posts test

---
+++ src/selectors/postSelectors.test.js
@@ -0,0 +1,22 @@
+import { getAllPosts } from './postSelectors';
+
+describe('post selectors', () => {
+  it('gets all blog posts', () => {
+    const state = [
+      {
+        title: 'Blog Post Uno',
+        body: 'blog post body texty text...'
+      },
+      {
+        title: 'Blog Post Dos',
+        body: 'second super cool text...'
+      }
+    ];
+
+    const posts = getAllPosts(state);
+    
+    expect(posts).toEqual(state);
+  }
+  );
+});
+
